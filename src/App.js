5d2f19fc239c44e291ceefc952aeb2205a1c82b1
yunji <nohyunji127@gmail.com>
2020-11-23 00:57:34 +0900

    sub add

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ function App() {
           rel="noopener noreferrer"
         > 
           안녕하세요 오늘은 11월 23일 입니다.
+         새로운 브랜치입니다. <br/>
+         마스터와 병합해주세요 !!!
         </a>
       </header>
     </div>
